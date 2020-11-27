1b43d5761e126539b109cdff31020509f528cce3
Joe Phinazee <joe.l.phinazee@gmail.com>
2020-11-27 12:06:26 -0800

    Abstracted Design Elements

--- hello.py
+++ hello.py
@@ -1,77 +1,36 @@
-import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
-class MyGridLayout(GridLayout):
-	# Initialize infinite keywords
-	def __init__(self, **kwargs):
-		# Call grid layout constructor
-		super(MyGridLayout, self).__init__(**kwargs)
-		
-		# Set columns
-		self.cols = 1
-		self.row_force_default=True
-		self.row_default_height=90
-		self.col_force_default=True
-		self.col_default_width=100
+class MyGridLayout(Widget):
+	
+	name = ObjectProperty(None)
+	pizza = ObjectProperty(None)
+	color = ObjectProperty(None)
 
-		# Create a second gridlayout
-		self.top_grid = GridLayout(
-			row_force_default=True,
-			row_default_height=40,
-			col_force_default=True,
-			col_default_width=100)
-		# Set number of columns in our new top_grid
-		self.top_grid.cols = 2
 
-		# Add widgets
-		self.top_grid.add_widget(Label(text="Name: "))
-		# Add Input Box
-		self.name = TextInput(multiline=False)
-		self.top_grid.add_widget(self.name)
-		# Add widgets
-		self.top_grid.add_widget(Label(text="Favorite Pizza: "))
-		# Add Input Box
-		self.pizza = TextInput(multiline=False)
-		self.top_grid.add_widget(self.pizza)
-		# Add widgets
-		self.top_grid.add_widget(Label(text="Favorite Color: "))
-		# Add Input Box
-		self.color = TextInput(multiline=False)
-		self.top_grid.add_widget(self.color)
-
-		# Add the new top_grid to our window
-		self.add_widget(self.top_grid)
-
-		# Create a Submit Button
-		self.submit = Button(text="Submit", font_size=32,
-			size_hint_y=None, height=50,
-			size_hint_x=None, width=200)
-		# Bind the button
-		self.submit.bind(on_press=self.press)
-		self.add_widget(self.submit)
-
-	def press(self, instance):
+	def press(self):
 		name = self.name.text
 		pizza = self.pizza.text
 		color = self.color.text
 
 		#print(f'Hello {name}, you like {pizza} pizza, and your favorite color is {color}!')
 		# Print it to the screen
-		self.add_widget(Label(text=f'Hello {name}, you like {pizza} pizza, and your favorite color is {color}!'))
-
-		# Clear the Input Boxes
+		#self.add_widget(Label(text=f'Hello {name}, you like {pizza} pizza, and your favorite color is {color}!'))
+		print(f'Hello {name}, you like {pizza} pizza, and your favorite color is {color}!')
+		# Clear the input boxes
 		self.name.text = ""
 		self.pizza.text = ""
 		self.color.text = ""
 
 class MyApp(App):
-	"""docstring for MyApp"""
 	def build(self):
 		return MyGridLayout()
+
 
 if __name__ == '__main__':
 	MyApp().run()
