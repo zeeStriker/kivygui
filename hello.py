a408722dedb48aacced234a70a62788a0a5e59cd
Joe Phinazee <joe.l.phinazee@gmail.com>
2020-11-28 06:06:15 -0800

    Added BoxLayout

--- hello.py
+++ hello.py
@@ -5,31 +5,13 @@ from kivy.lang import Builder
 
 Builder.load_file('whatever.kv')
 
-class MyGridLayout(Widget):
-	
-	name = ObjectProperty(None)
-	pizza = ObjectProperty(None)
-	color = ObjectProperty(None)
+class MyLayout(Widget):
+	pass
 
-
-	def press(self):
-		name = self.name.text
-		pizza = self.pizza.text
-		color = self.color.text
-
-		#print(f'Hello {name}, you like {pizza} pizza, and your favorite color is {color}!')
-		# Print it to the screen
-		#self.add_widget(Label(text=f'Hello {name}, you like {pizza} pizza, and your favorite color is {color}!'))
-		print(f'Hello {name}, you like {pizza} pizza, and your favorite color is {color}!')
-		# Clear the input boxes
-		self.name.text = ""
-		self.pizza.text = ""
-		self.color.text = ""
 
 class AwesomeApp(App):
 	def build(self):
-		return MyGridLayout()
-
+		return MyLayout()
 
 if __name__ == '__main__':
 	AwesomeApp().run()
