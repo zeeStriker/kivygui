a034e4fb6a360c13b83b34adc4faaa5358fc181d
Joe Phinazee <joe.l.phinazee@gmail.com>
2020-12-01 04:06:41 -0800

    Updated Labels

--- hello.py
+++ hello.py
@@ -2,17 +2,22 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-from kivy.core.window import Window
 
 Builder.load_file('whatever.kv')
 
 class MyLayout(Widget):
-	pass
+	def press(self):
+		# Create variables for our widget
+		name = self.ids.name_input.text
 
+		# Update the label
+		self.ids.name_label.text = f'Hello {name}!'
+
+		# Clear input box
+		self.ids.name_input.text = ''
 
 class AwesomeApp(App):
 	def build(self):
-		Window.clearcolor = (1, 1, 1, 1)
 		return MyLayout()
 
 if __name__ == '__main__':
