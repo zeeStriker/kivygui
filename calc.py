01da28d0856ee8d50a6f5ae936a1d4c676fe215e
Joe Phinazee <joe.l.phinazee@gmail.com>
2020-12-02 12:55:43 -0800

    finished addition if statement

--- calc.py
+++ calc.py
@@ -12,6 +12,63 @@ Builder.load_file('calc.kv')
 class MyLayout(Widget):
 	def clear(self):
 		self.ids.calc_input.text = '0'
+
+	# Create a button pressing function
+	def button_press(self, button):
+		# Create a variable that was previously in the text box
+		prior = self.ids.calc_input.text
+
+		# Determine if 0 is there
+		if prior == '0':
+				self.ids.calc_input.text = ''
+				self.ids.calc_input.text = f'{button}'
+		else:
+			self.ids.calc_input.text = f'{prior}{button}'
+
+	# Create addition function
+	def add(self):
+		# Create a variable that was previously in the text box
+		prior = self.ids.calc_input.text
+
+		self.ids.calc_input.text = f'{prior}+'
+
+	# Create subtraction function
+	def subtract(self):
+		# Create a variable that was previously in the text box
+		prior = self.ids.calc_input.text
+
+		self.ids.calc_input.text = f'{prior}-'
+
+	# Create multiplication function
+	def multiply(self):
+		# Create a variable that was previously in the text box
+		prior = self.ids.calc_input.text
+
+		self.ids.calc_input.text = f'{prior}*'
+
+	# Create division function
+	def divide(self):
+		# Create a variable that was previously in the text box
+		prior = self.ids.calc_input.text
+
+		self.ids.calc_input.text = f'{prior}/'
+
+	# Create equals function
+	def equals(self):
+		# Create a variable that was previously in the text box
+		prior = self.ids.calc_input.text
+
+		# Addition
+		if '+' in prior:
+			num_list = prior.split("+")
+			answer = 0
+
+			# loop through our list
+			for number in num_list:
+				answer = answer + int(number)
+
+			# print the answer to text box
+			self.ids.calc_input.text = str(answer)
 	
 class CalculatorApp(App):
 	def build(self):
